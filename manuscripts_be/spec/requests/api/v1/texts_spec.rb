7f3ff4709ad89a6095b73bc833d7915801223d91
ferrisbueller66 <tmtarpinian@gmail.com>
2021-05-18 23:51:03 -0400

    write initial request specs

--- manuscripts_be/spec/requests/api/v1/texts_spec.rb
+++ manuscripts_be/spec/requests/api/v1/texts_spec.rb
@@ -1,7 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Texts", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  
+  describe "GET /texts" do
+    before(:context) { get api_v1_texts_path }  # get(:index)
+    
+    it "is a success" do
+      expect(response).to have_http_status(:ok)
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+    end
   end
 end
