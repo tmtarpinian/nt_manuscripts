bcd2255ab88cf16c3da94288525d30438e1bb3ab
ferrisbueller66 <tmtarpinian@gmail.com>
2021-07-25 22:19:45 -0400

    add strong params and helper to reference controller

--- manuscripts_be/app/controllers/api/v1/references_controller.rb
+++ manuscripts_be/app/controllers/api/v1/references_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::ReferencesController < ApplicationController
+    before_action :set_reference, only: [:show]
+
     def index
         references = Reference.all
         render json: references
@@ -6,8 +8,21 @@ class Api::V1::ReferencesController < ApplicationController
     end
 
     def show
-        reference = Reference.find(params[:id])
-        render json: reference
-        # render json: {reference: ReferenceSerializer.new(@reference), text: TextSerializer.new(@reference.texts)}, status: :created
+        # render json: {reference: ReferenceSerializer.new(@reference), texts: TextSerializer.new(@reference.texts)}, status: :created
+        if @reference
+            render json: @reference, status: :created
+        else
+            render json: @reference.errors, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def set_reference(book = params[:book], chapter=params[:chapter], verse=params[:verse])
+        @reference = Reference.find_by(book: book, chapter: chapter, verse: verse)
+    end
+
+     def reference_params
+        params.permit(:id, :book, :chapter, :verse)
     end
 end
