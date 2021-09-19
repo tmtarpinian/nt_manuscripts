afc5c502d0c1027d431c99715782d62156a40e83
ferrisbueller66 <tmtarpinian@gmail.com>
2021-09-19 12:43:01 -0400

    update model annotations

--- manuscripts_be/spec/models/reference_text_spec.rb
+++ manuscripts_be/spec/models/reference_text_spec.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: reference_texts
+#
+#  id               :integer          not null, primary key
+#  text_id          :integer          not null
+#  reference_id     :integer          not null
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe ReferenceText, type: :model do
